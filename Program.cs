f373ea74c2298a5367b05ce6e33299e3fc9af008
Daniel <danielwidegren@gmail.com>
2025-09-10 18:39:30 +0000

    stream audio file from api endpoint

--- Program.cs
+++ Program.cs
@@ -14,28 +14,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var path = Path.Combine(app.Environment.ContentRootPath, "files", "magnolia-blues.wav");
+    if (!File.Exists(path))
+    {
+        return Results.NotFound("Song not found.");
+    }
+    var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+    var mimeType = "audio/wav";
+    return Results.File(path, contentType: mimeType, fileDownloadName: "magnolia.wav");
 })
 .WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
